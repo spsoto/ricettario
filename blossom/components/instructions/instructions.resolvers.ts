9bcd0e7f79eb183756d921d16910fa81ff28990b
Sebastian Soto <spsoto@users.noreply.github.com>
2019-06-30 14:29:35 -0700

    Add relations between recipes and instructions

--- blossom/components/instructions/instructions.resolvers.ts
+++ blossom/components/instructions/instructions.resolvers.ts
@@ -1,24 +1,38 @@
 import { Resolver } from '@blossom-gql/core';
 
-import { RequestContext } from 'blossom/instance';
+import { resolve, RequestContext } from 'blossom/instance';
 import { Instruction } from 'blossom/components/instructions/instructions.types';
 import InstructionModel from 'lib/models/instruction.model';
+import { recipeById } from '../recipes/recipes.sources';
+import { recipeResolver } from '../recipes/recipes.resolvers';
 
 export const instructionResolver: Resolver<
   InstructionModel,
   Instruction,
   RequestContext
 > = function instructionResolver(attributes) {
+  const recipeId = attributes.recipeId.toString();
+
   return {
     // Must always be present.
     __typename: 'Instruction',
     id: attributes.id.toString(),
-    recipeId: attributes.recipeId.toString(),
+    recipeId,
     instructionContent: attributes.instructionContent,
     createdAt: attributes.createdAt.getTime(),
     updatedAt: attributes.updatedAt.getTime(),
-    async recipe() {
-      throw new Error('not implemented yet');
+    async recipe(_, ctx, ast) {
+      const recipe = await ctx.loader(recipeById).load(recipeId);
+      if (!recipe) {
+        throw new Error(`Recipe with id ${recipeId} not found.`);
+      }
+
+      return resolve({
+        data: recipe,
+        ctx,
+        using: recipeResolver,
+        ast,
+      });
     },
   };
 };
--- blossom/components/recipes/recipes.resolvers.ts
+++ blossom/components/recipes/recipes.resolvers.ts
@@ -1,18 +1,25 @@
 import { Resolver } from '@blossom-gql/core';
 
-import { RequestContext } from 'blossom/instance';
+import { RequestContext, resolveArray } from 'blossom/instance';
 import { Recipe } from 'blossom/components/recipes/recipes.types';
 
 import RecipeModel from 'lib/models/recipe.model';
+import InstructionModel from 'lib/models/instruction.model';
+
+import { instructionByRecipeId } from '../instructions/instructions.sources';
+import { instructionResolver } from '../instructions/instructions.resolvers';
+import { Instruction } from '../instructions/instructions.types';
 
 export const recipeResolver: Resolver<
   RecipeModel,
   Recipe,
   RequestContext
 > = function recipeResolver(attributes) {
+  const id = attributes.id.toString();
+
   return {
     __typename: 'Recipe',
-    id: attributes.id.toString(),
+    id,
     title: attributes.title,
     description: attributes.description,
     createdAt: attributes.createdAt.getTime(),
@@ -20,9 +27,15 @@ export const recipeResolver: Resolver<
     async recipeIngredients() {
       return [];
     },
-    async instructions() {
-      return [];
+    async instructions(_, ctx, ast) {
+      const instructions = await ctx.loader(instructionByRecipeId).load(id);
+
+      return resolveArray({
+        data: instructions,
+        ctx,
+        using: instructionResolver,
+        ast,
+      });
     },
-    // TODO: Remove this and map attributes to the properties of the output type.
   };
 };
