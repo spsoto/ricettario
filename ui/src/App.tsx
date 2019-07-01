7ab8c6aafcf46adf6409657bd4ff75fad7b97859
Sebastian Soto <spsoto@users.noreply.github.com>
2019-06-30 22:18:44 -0700

    Add prettier

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,24 +1,70 @@
-import React from 'react'
+import React from 'react';
 
-import { Navbar, NavbarBrand } from 'reactstrap'
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 const App = () => (
   <div>
     <Navbar color="dark" dark expand="md" fixed="top" className="justify-content-center">
       <NavbarBrand>
-        <span role="img" aria-label="Chef Female">👩‍🍳</span>{" "}
-        Ricettario{" "}
-        <span role="img" aria-label="Chef Male">👨🏻‍🍳</span>
+        <span role="img" aria-label="Chef Female">
+          👩‍🍳
+        </span>{' '}
+        Ricettario{' '}
+        <span role="img" aria-label="Chef Male">
+          👨🏻‍🍳
+        </span>
       </NavbarBrand>
     </Navbar>
     <div className="container">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sodales, neque eu ultricies aliquet, nisl lacus aliquet nulla, at placerat diam quam in lectus. Quisque blandit sapien sed varius porta. Nullam lectus magna, placerat sed diam gravida, pharetra consequat quam. Mauris iaculis lorem a lacinia vehicula. Ut aliquam ex id aliquam semper. Donec gravida ultrices arcu, id vestibulum sapien blandit a. Ut auctor leo id arcu dignissim hendrerit. Vivamus hendrerit orci eros, id aliquam sapien ullamcorper eget. Pellentesque eu gravida orci. Proin sodales ligula dolor, a venenatis purus elementum ac. Proin fringilla vel urna eget luctus. Integer tincidunt semper metus, nec congue lectus hendrerit hendrerit. Vivamus rhoncus metus eget justo accumsan pretium.</p>
-      <p>Donec id rutrum metus, vitae commodo quam. Nulla laoreet lorem sit amet leo accumsan consequat blandit at dui. Donec convallis felis ut diam suscipit, eget dapibus velit venenatis. Ut vel finibus nunc. Quisque sit amet ipsum porttitor, placerat ipsum non, congue augue. Nunc pellentesque malesuada felis sit amet interdum. Sed tincidunt sapien ut odio iaculis, sit amet imperdiet dui suscipit. Phasellus laoreet sagittis tortor, malesuada molestie massa viverra nec. Donec nec convallis neque, id rhoncus libero. Phasellus pharetra luctus ligula quis hendrerit. In consectetur auctor eleifend.</p>
-      <p>Cras cursus libero egestas consequat iaculis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam id convallis dui, ac sodales velit. Cras sit amet elit et sem rutrum pharetra. Proin vel justo in nunc tincidunt euismod. Maecenas porta dapibus magna laoreet fermentum. In et risus eu purus consectetur pulvinar id ut ligula. In at dapibus mauris, id fringilla risus. Nam congue maximus ipsum. Nunc ut malesuada ante. Sed sem leo, pretium nec ipsum a, aliquet vestibulum ligula. Maecenas tempor eros suscipit felis tincidunt, sed sollicitudin tellus pellentesque. Curabitur vestibulum sit amet lorem ac placerat. Morbi sit amet orci eu ipsum lobortis lacinia eu a metus. Donec sed tristique mauris. Vestibulum nunc eros, placerat eu tincidunt id, lobortis blandit purus.</p>
-      <p>In consequat magna ac lacus auctor commodo. Cras faucibus interdum ullamcorper. In et mi ullamcorper, vulputate felis id, finibus turpis. Quisque nisl lacus, pretium ut mi et, blandit eleifend dui. Cras mollis, tellus eu tincidunt dignissim, nunc elit venenatis neque, ac fringilla quam ipsum lacinia urna. Curabitur sit amet feugiat ligula, vitae molestie leo. Donec nec condimentum arcu.</p>
-      <p>Morbi mollis velit dolor, vel interdum urna dictum nec. Fusce ac pulvinar neque. Pellentesque a neque vitae tortor hendrerit viverra. Sed tellus libero, bibendum et semper pharetra, mollis vitae purus. Suspendisse a mattis dolor. Sed placerat quis arcu at consectetur. Curabitur sodales dolor id augue varius finibus. Etiam pulvinar mi in nisi lacinia, quis semper ligula iaculis. Ut eget fermentum elit. Aliquam auctor nulla dignissim dictum semper. Suspendisse quis tortor ac ligula volutpat blandit. </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sodales, neque eu ultricies
+        aliquet, nisl lacus aliquet nulla, at placerat diam quam in lectus. Quisque blandit sapien
+        sed varius porta. Nullam lectus magna, placerat sed diam gravida, pharetra consequat quam.
+        Mauris iaculis lorem a lacinia vehicula. Ut aliquam ex id aliquam semper. Donec gravida
+        ultrices arcu, id vestibulum sapien blandit a. Ut auctor leo id arcu dignissim hendrerit.
+        Vivamus hendrerit orci eros, id aliquam sapien ullamcorper eget. Pellentesque eu gravida
+        orci. Proin sodales ligula dolor, a venenatis purus elementum ac. Proin fringilla vel urna
+        eget luctus. Integer tincidunt semper metus, nec congue lectus hendrerit hendrerit. Vivamus
+        rhoncus metus eget justo accumsan pretium.
+      </p>
+      <p>
+        Donec id rutrum metus, vitae commodo quam. Nulla laoreet lorem sit amet leo accumsan
+        consequat blandit at dui. Donec convallis felis ut diam suscipit, eget dapibus velit
+        venenatis. Ut vel finibus nunc. Quisque sit amet ipsum porttitor, placerat ipsum non, congue
+        augue. Nunc pellentesque malesuada felis sit amet interdum. Sed tincidunt sapien ut odio
+        iaculis, sit amet imperdiet dui suscipit. Phasellus laoreet sagittis tortor, malesuada
+        molestie massa viverra nec. Donec nec convallis neque, id rhoncus libero. Phasellus pharetra
+        luctus ligula quis hendrerit. In consectetur auctor eleifend.
+      </p>
+      <p>
+        Cras cursus libero egestas consequat iaculis. Pellentesque habitant morbi tristique senectus
+        et netus et malesuada fames ac turpis egestas. Nam id convallis dui, ac sodales velit. Cras
+        sit amet elit et sem rutrum pharetra. Proin vel justo in nunc tincidunt euismod. Maecenas
+        porta dapibus magna laoreet fermentum. In et risus eu purus consectetur pulvinar id ut
+        ligula. In at dapibus mauris, id fringilla risus. Nam congue maximus ipsum. Nunc ut
+        malesuada ante. Sed sem leo, pretium nec ipsum a, aliquet vestibulum ligula. Maecenas tempor
+        eros suscipit felis tincidunt, sed sollicitudin tellus pellentesque. Curabitur vestibulum
+        sit amet lorem ac placerat. Morbi sit amet orci eu ipsum lobortis lacinia eu a metus. Donec
+        sed tristique mauris. Vestibulum nunc eros, placerat eu tincidunt id, lobortis blandit
+        purus.
+      </p>
+      <p>
+        In consequat magna ac lacus auctor commodo. Cras faucibus interdum ullamcorper. In et mi
+        ullamcorper, vulputate felis id, finibus turpis. Quisque nisl lacus, pretium ut mi et,
+        blandit eleifend dui. Cras mollis, tellus eu tincidunt dignissim, nunc elit venenatis neque,
+        ac fringilla quam ipsum lacinia urna. Curabitur sit amet feugiat ligula, vitae molestie leo.
+        Donec nec condimentum arcu.
+      </p>
+      <p>
+        Morbi mollis velit dolor, vel interdum urna dictum nec. Fusce ac pulvinar neque.
+        Pellentesque a neque vitae tortor hendrerit viverra. Sed tellus libero, bibendum et semper
+        pharetra, mollis vitae purus. Suspendisse a mattis dolor. Sed placerat quis arcu at
+        consectetur. Curabitur sodales dolor id augue varius finibus. Etiam pulvinar mi in nisi
+        lacinia, quis semper ligula iaculis. Ut eget fermentum elit. Aliquam auctor nulla dignissim
+        dictum semper. Suspendisse quis tortor ac ligula volutpat blandit.{' '}
+      </p>
     </div>
   </div>
-)
+);
 
-export default App
+export default App;
