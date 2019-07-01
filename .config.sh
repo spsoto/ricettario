0982fdd7a6a2aec513ed60664724f47b64480ca1
Sebastian Soto <spsoto@users.noreply.github.com>
2019-06-30 22:13:17 -0700

    Add some UI components

--- .config.sh
+++ .config.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 export PROJECT_NAME=ricettario
+
+trap 'docker-compose -p ricettario down' SIGINT EXIT
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>Hello!</div>, document.getElementById('root'));
+import App from './App'
+
+import './index.css'
+
+ReactDOM.render(<App/>, document.getElementById('root'));
